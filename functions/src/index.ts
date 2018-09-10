808a269358581dba44cb2731c47ad4f5c24b9a15
whtrbit <kontakt@grzegorzbielak.pl>
2018-09-10 23:04:36 +0200

    Temperature module in its own file

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,20 +1,3 @@
-import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
+import {getTemperature} from "./temperature";
 
-admin.initializeApp();
-
-const db = admin.firestore();
-
-exports.getTemperatures = functions.https.onRequest(async (request, response) => {
-    try {
-        response.status(200).send(await getTemperatures());
-    } catch (e) {
-        response.status(500).send(e);
-    }
-});
-
-async function getTemperatures(): Promise<any[]> {
-    return (await db.collection('/temperatures').get()).docs.map(val => {
-        return val.data();
-    });
-}
+exports.getTemperatures = getTemperature;
---
+++ functions/src/temperature.ts
@@ -0,0 +1,20 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
+
+const db = admin.firestore();
+
+export const getTemperature = functions.https.onRequest(async (request, response) => {
+    try {
+        response.status(200).send(await getTemperatures());
+    } catch (e) {
+        response.status(500).send(e);
+    }
+});
+
+async function getTemperatures(): Promise<any[]> {
+    return (await db.collection('/temperatures').get()).docs.map(val => {
+        return val.data();
+    });
+}
